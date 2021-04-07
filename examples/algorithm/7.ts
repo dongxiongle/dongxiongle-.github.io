c033b738bb8c08e28fea3c6fdabaa6e5efe830a4
杨章波 <yangzhangbo@91jinrong.com>
2021-04-07 18:27:08 +0800

    快速排序

--- examples/algorithm/7.ts
+++ examples/algorithm/7.ts
@@ -1,58 +1,63 @@
 /**
- * 划分操作
- * @param target 选择的数组
- * @param left 数组最左边
- * @param right 数组的最右边
- * @returns 划分操作后的数组
+ * 快速排序入口
+ * @param arr 待排序的数组
  */
-function partition<T>(target: T[], left: number, right: number) {
-  const pivot = target[Math.floor((left + right) / 2)];
-  let i = left; 
+function quickSort<T>(arr: T[]) {
+  return quick(arr, 0, arr.length - 1);
+}
+/**
+ * 排序递归函数
+ * @param arr 待排序的数组
+ * @param left 最左侧指针
+ * @param right 最右侧指针
+ * @returns 排序后的数组
+ */
+function quick<T>(arr: T[], left: number, right: number) {
+  let index: number;
+  if (arr.length > 1) {
+    // 划分，获取指针
+    index = partition(arr, left, right);
+    // 对比主元小的子数组排序
+    if (index - 1 > left) {
+      quick(arr, left, index - 1);
+    }
+    // 对比主元大的子数组排序
+    if (index < right) {
+      quick(arr, index, right);
+    }
+  }
+  return arr;
+}
+/**
+ * 划分操作
+ * @param arr 待排序数组
+ * @param left 左侧指针
+ * @param right 右侧指针
+ * @returns 左侧指针
+ */
+function partition<T>(arr: T[], left: number, right: number) {
+  const pivot = arr[Math.floor((left + right) / 2)];
+  let i = left;
   let j = right;
-  while(i <= j) {
-    while(target[i] < pivot) {
+  // 直到左侧指针超过右侧指针
+  while (i <= j) {
+    // 左侧，找到比主元大的值
+    while (arr[i] < pivot) {
       i++;
     }
-    while(target[j] > pivot) {
+    // 右侧，找到比主元小的值
+    while (arr[j] > pivot) {
       j--;
     }
+    // 交换
     if (i <= j) {
-      const temp = target[i];
-      target[i] = target[j];
-      target[j] = temp;
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
       i++;
       j--;
     }
   }
   return i;
 }
-/**
- * 递归函数
- * @param target 选择的数组
- * @param left 数组的最左边
- * @param right 数组的最右边
- * @returns 排序后的数组
- */
-function quick<T>(target: T[], left: number, right: number) {
-  let index: number;
-  if(target.length > 1) {
-    index = partition(target, left, right);
-    if (left < index - 1) {
-      quick(target, left, index - 1);
-    }
-    if (left < right) {
-      quick(target, index, right);
-    }
-  }
-  return target;
-}
-/**
- * 快速排序入口
- * @param target 要排序的数组
- * @returns 排序后的数组
- */
-function quickSort<T>(target: T[]) {
-  const arr = target;
-  return quick(arr, 0, arr.length - 1);
-}
-console.log(quickSort([5, 2, 3, 6, 1]));
+console.log(quickSort([5, 2, 1, 3, 4, 6, 1, 6, 9, 7]));
